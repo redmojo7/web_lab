d1546687e6e05f60e45a6648f18918698bdd8861
redmojo <donaldchoi5180@gmail.com>
2023-03-13 14:43:59 +0800

    To connect React with Back-End Node.js

--- api/app.js
+++ api/app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
-  res.send('Home Route');
+  //res.send('This Back-End Home Route');
+  res.json({ message: "This Back-End Home Route!" });
 });
+
+app.get('/express_backend', (req, res) => { //Line 9
+  res.json({ message: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+  //res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+});
+
 app.listen(port, () =>
   console.log(`Server running on port ${port}, http://localhost:${port}`)
 );
--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,24 @@
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8080/express_backend")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{message}</h1>
     </div>
   );
 }
 
-export default App;
+export default App
