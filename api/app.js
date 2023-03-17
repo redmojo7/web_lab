4c780749ccb480a3d8228ab5e4a76aaf9ecc8738
redmojo <donaldchoi5180@gmail.com>
2023-03-17 21:05:10 +0800

    Integrate PostgreSQL into this Docker Compose file

--- api/app.js
+++ api/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const cors = require("cors");
+const { Pool } = require('pg');
+
 const app = express();
 const port = process.env.PORT || 8080;
+
 app.use(cors());
 app.use(express.json());
 
@@ -13,6 +16,31 @@ app.get('/', (req, res) => {
 
 app.get('/express_backend', (req, res) => { //Line 9
   res.json({ message: "YOUR EXPRESS BACKEND IS CONNECTED TO REACT" });
+  //res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
+});
+
+// test for PostgreSQL
+
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432, // default port for PostgreSQL
+});
+
+
+app.get('/db', (req, res) => {
+  pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('PostgreSQL connected:', res.rows[0].now);
+    pool.end();
+  });
+  res.json({ message: "PostgreSQL connected:"});
   //res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 });
 
